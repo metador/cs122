b12513110dc8bd04392ca761972a1afad1e73b09
mukhtar89 <muhammad.28.1989@gmail.com>
2015-04-05 01:20:44 -0700

    Query+MetaData

--- src/JDBC.java
+++ src/JDBC.java
@@ -5,47 +5,87 @@ public class JDBC
 {
 	public static Connection connection = null;
 	public static String Username = "root", Password = "muk5086";
-	public static Scanner stringscan = null;
+	public static Scanner stringscan = null, intscan = null;
+	public static int choice = 0;
+	public static Metadata metadata = null;
+	public static Query cmdline = null;
+	public static Statement statement = null;
+	public static String exiting = null;
 	
 	public static void main (String arg[]) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
 	{
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		
 		connection = DriverManager.getConnection("jdbc:mysql:///moviedb", Username, Password);
-		//while(login());
-		Menu();
 		
-		Statement select = connection.createStatement();
+		do
+		{
+			while(login());
+			while(Menu());
+			System.out.println("\n\tDo you want to Exit the program? ['Yes' or 'No'] : ");
+			stringscan = new Scanner(System.in);
+			exiting = stringscan.nextLine();
+		} while (!exiting.equals("Yes"));
 		
-		Operat operation = new Operat(select);
 		
-		operation.Select();
+		
+		//operation.Select();
 		
 		
 	}
 	
-	public static boolean login()
+	public static boolean login() throws SQLException
 	{
 		stringscan = new Scanner(System.in);
-		System.out.println("-----------------LOGIN---------------------");
+		System.out.println("\n-----------------LOGIN---------------------");
 		System.out.println("\n\tUsername = ");
 		Username = stringscan.nextLine();
 		System.out.println("\n\tPassword = ");
 		Password = stringscan.nextLine();
-		return false;
+		statement = connection.createStatement();
+		String command = "Select * from users where username = '" + Username + "' and password = '" + Password + "'";
+		ResultSet result = statement.executeQuery(command);
+		int columns = result.getMetaData().getColumnCount();
+		if (result.next())
+			return false;
+		else
+			return true;
 	}
 	
-	public static void Menu ()
+	public static boolean Menu () throws SQLException
 	{
-		System.out.println("-----------------MENU---------------------");
+		System.out.println("\n-----------------MENU---------------------");
 		System.out.println("\n\t1. Print All movies grouped by stars");
 		System.out.println("\n\t2. Search for a Movie Star");
 		System.out.println("\n\t3. Search for Movies acted by a Star");
 		System.out.println("\n\t4. Insert a Star");
 		System.out.println("\n\t5. Add a new Customer");
 		System.out.println("\n\t6. Delete existing customer");
-		System.out.println("\n\t7. Exit the Menu");
-		System.out.println("\n\t8. Exit the program");
-		System.out.println("\n\n\t\t :");
+		System.out.println("\n\t7. Display the MetaData of Database");
+		System.out.println("\n\t8. Query the Database");
+		System.out.println("\n\t9. Exit the Menu");
+		System.out.println("\n\t10. Exit the program");
+		System.out.println("\n\n\t\tEnter your choice :");
+		intscan = new Scanner(System.in);
+		choice = intscan.nextInt();
+		
+		switch(choice)
+		{
+			case 7: 
+				metadata = new Metadata(connection);
+				Metadata.print();
+				break;
+			case 8: 
+				cmdline = new Query(connection);
+				cmdline.display();
+				break;
+			case 9:
+				return false;
+			case 10:
+				return false;
+			default:
+				break;
+		}
+		return true;
+		
 	}
 }
